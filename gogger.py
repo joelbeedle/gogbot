b019ad9269ce1926ffc8a87880029bb0ede1cd2c
joelbeedle <beedlejoel@gmail.com>
2021-04-27 20:49:53 +0100

    Removed useless functions
    After the individual naming functions are now obselete due to
    quote() taking over, they aren't needed as commands anymore.

--- gogger.py
+++ gogger.py
@@ -42,41 +42,6 @@ async def quote(ctx, person: str):
     await ctx.send(response)
 
 
-@bot.command(name='fred')
-async def fred(ctx):
-    fred_quotes = ['dude', 'wanna try my dubai tobacco', '*inhales vape*']
-    response = random.choice(fred_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name='crocker')
-async def crocker(ctx):
-    crocker_quotes = ['Chemistry', 'elliot', 'gog', 'joji']
-    response = random.choice(crocker_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name='matt')
-async def matt(ctx):
-    matt_quotes = ['Chemistry', 'drink', 'gogger', 'gog', '🤙🤙']
-    response = random.choice(matt_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name='jude')
-async def jude(ctx):
-    jude_quotes = ['rampage', 'mysterious', 'girl']
-    response = random.choice(jude_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name='joel')
-async def joel(ctx):
-    joel_quotes = ['ay']
-    response = random.choice(joel_quotes)
-    await ctx.send(response)
-
-
 @bot.command(name='gog', help='Pray to Gog and receive his reply.')
 async def gog(ctx):
     gog_list = ['gog ', 'gogger ', 'gogging ', 'gogged ',
